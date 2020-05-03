0d378d42f93f3db62410ef0836a2913be7de1aef
Jens Klingenberg <mail@jensklingenberg.de>
2020-05-03 19:25:19 +0200

    init commit

--- web/webpack.config.d/minify.js
+++ web/webpack.config.d/minify.js
@@ -1,7 +1,13 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-  optimization: {
-    minimizer: [new UglifyJsPlugin()],
-  },
+// dev server
+config.devServer = {
+  "inline": true,
+  "lazy": false,
+  "noInfo": true,
+  "open": true,
+  "overlay": false,
+  "port": 8083,
+  "contentBase": [
+    "/home/jens/Code/2020/JK/orpso/web/build/processedResources/Js/main"
+  ]
 };
